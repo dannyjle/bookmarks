5d7e1a6457398376e64f8b778d052abea59515f1
Danny Le <le.dannyj@gmail.com>
2021-10-11 11:57:45 -0400

    Created tables for books and reviews in migrations

--- db/migrate/20211011153540_create_reviews.rb
+++ db/migrate/20211011153540_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-
+      t.string :author
+      t.string :comment
+      t.references :book
+      # The references column is needed because the Review model has a belongs_to association with the Book model.
       t.timestamps
     end
   end
